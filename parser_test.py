5f859e3279f4496ee82b9ae9c600261e77bd8074
Epholys <epholys@gmail.com>
2020-11-15 19:03:23 +0100

    Reorganize and complete test

--- parser_test.py
+++ parser_test.py
@@ -4,49 +4,83 @@ import unittest
 import main
 
 
-class TestParseDate(unittest.TestCase):
-    def assert_almost_eq(self, actual, expected, message=""):
-        self.assertAlmostEqual(
-            actual, expected, delta=dt.timedelta(microseconds=100), msg=message
-        )
+def _assert_dt_almost_eq(actual, expected, message=""):
+    unittest.TestCase().assertAlmostEqual(
+        actual, expected, delta=dt.timedelta(microseconds=100), msg=message
+    )
 
+
+class TestParseTime(unittest.TestCase):
     def test_HH(self):
         HH = 19
-        today = dt.date.today()
-        time = dt.time(HH)
-        expected = dt.datetime.combine(today, time)
-        parsed = main.parse_datetime(str(HH))
-        self.assert_almost_eq(parsed, expected)
+        expected = dt.time(HH)
+
+        user_input = f"{HH:02}"
+        parsed = main.parse_time(user_input)
+        _assert_dt_almost_eq(parsed, expected)
 
     def test_HHMM(self):
         HH = 4
         MM = 6
-        time_input = f"0{HH}:0{MM}"
-        today = dt.date.today()
+        expected = dt.time(HH, MM)
+
+        user_input = f"{HH:02}:{MM:02}"
+        parsed = main.parse_time(user_input)
+        _assert_dt_almost_eq(parsed, expected)
+
+    def test_incorrect(self):
+        parsed = main.parse_time("89:9")
+        self.assertIsNone(parsed)
+
+
+class TestParseDate(unittest.TestCase):
+    def test_mmdd(self):
+        mm = 10
+        dd = 12
+        expected = dt.date(dt.date.today().year, mm, dd)
+
+        user_input = f"{mm:02}-{dd:02}"
+        parsed = main.parse_date(user_input)
+        _assert_dt_almost_eq(parsed, expected)
+
+    def test_incorrect(self):
+        parsed = main.parse_time("9-19")
+        self.assertIsNone(parsed)
+
+
+class TestParseDateTime(unittest.TestCase):
+    def test_HHMM(self):
+        HH = 14
+        MM = 26
+        date = dt.date.today()
         time = dt.time(HH, MM)
-        expected = dt.datetime.combine(today, time)
-        parsed = main.parse_datetime(time_input)
-        self.assert_almost_eq(parsed, expected)
+        expected = dt.datetime.combine(date, time)
+
+        user_input = f"{HH:02}:{MM:02}"
+        parsed = main.parse_datetime(user_input)
+        _assert_dt_almost_eq(parsed, expected)
 
     def test_mmdd(self):
-        mm = 12
-        dd = 31
-        time_input = f"{mm}-{dd}"
-        expected_time = dt.datetime.now().time()
-        expected_date = dt.date(dt.date.today().year, mm, dd)
-        expected = dt.datetime.combine(expected_date, expected_time)
-        parsed = main.parse_datetime(time_input)
-        self.assert_almost_eq(parsed, expected)
+        mm = 10
+        dd = 12
+        date = dt.date(dt.date.today().year, mm, dd)
+        time = dt.datetime.now().time()
+        expected = dt.datetime.combine(date, time)
+
+        user_input = f"{mm:02}-{dd:02}"
+        parsed = main.parse_datetime(user_input)
+        _assert_dt_almost_eq(parsed, expected)
 
     def test_mmddHHmm(self):
         mm = 2
         dd = 28
         HH = 4
         MM = 6
-        time_input = f"0{mm}-{dd} 0{HH}:0{MM}"
         expected = dt.datetime(dt.date.today().year, mm, dd, HH, MM)
-        parsed = main.parse_datetime(time_input)
-        self.assert_almost_eq(parsed, expected)
+
+        input_time = f"{mm:02}-{dd:02} {HH:02}:{MM:02}"
+        parsed = main.parse_datetime(input_time)
+        _assert_dt_almost_eq(parsed, expected)
 
     def test_incorrect(self):
         parsed = main.parse_datetime("IO:54 ZD-LK")
